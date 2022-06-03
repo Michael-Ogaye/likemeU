43fd021fb85f7c178a78512db9dea66cf7467589
Michael Ogaye <hogaeymic@gmail.com>
2022-06-03 22:05:14 +0300

    ft:profile model created

--- likeme/settings.py
+++ likeme/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config, Csv
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
