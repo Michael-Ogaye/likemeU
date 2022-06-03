b605008123b886e8980147fb04f89eb20aee08d2
Michael Ogaye <hogaeymic@gmail.com>
2022-06-04 00:38:23 +0300

    urlpatterns

--- plata/urls.py
+++ plata/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-# from . import views
+from django.urls import path,include
+from . import views
 
-# urlpatterns = [
-#    path('',views.index, name='home')
-# ]
+urlpatterns = [
+    path('',views.index, name='home'),
+    path('signup/', views.signup, name='signup'),
+    path('account/', include('django.contrib.auth.urls')),
+]
