651175003d9fce6c4f33c0ee287a6a2bd61233fd
Michael Ogaye <hogaeymic@gmail.com>
2022-06-03 23:39:35 +0300

    signup view

--- likeme/settings.py
+++ likeme/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'plata',
+    'crispy_forms',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
