eebfeff319ac82371aefd33499bbbd82183e3db5
Michael Ogaye <hogaeymic@gmail.com>
2022-06-04 13:02:22 +0300

    follow and unfollow views

--- plata/views.py
+++ plata/views.py
@@ -95,3 +95,35 @@ def user_profile(request, username):
     
     return render(request, 'plata/user_profile.html', context)
 
+
+def unfollow(request, to_unfollow):
+    if request.method == 'GET':
+        user_profile2 = Profile.objects.get(pk=to_unfollow)
+        unfollow_d = Follow.objects.filter(follower=request.user.profile, followed=user_profile2)
+        unfollow_d.delete()
+        return redirect('user_profile', user_profile2.user.username)
+
+
+def follow(request, to_follow):
+    if request.method == 'GET':
+        user_profile3 = Profile.objects.get(pk=to_follow)
+        follow_s = Follow(follower=request.user.profile, followed=user_profile3)
+        follow_s.save()
+        return redirect('user_profile', user_profile3.user.username)
+
+
+@login_required(login_url='login')
+def search_profile(request):
+    if 'search_user' in request.GET and request.GET['search_user']:
+        name = request.GET.get("search_user")
+        results = Profile.search_profile(name)
+        print(results)
+        message = f'name'
+        context = {
+            'results': results,
+            'message': message
+        }
+        return render(request, 'plata/results.html', context)
+    else:
+        message = "You haven't searched for any image category"
+    return render(request, 'plata/results.html', {'message': message})
